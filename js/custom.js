fddf15a78d5e183837673112b4c9b023ddea6b46
Bekzod <bek.bek.bek.bek72@gmail.com>
2021-01-30 14:05:01 +0500

    scripts from index.html replaced to custom.js

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,52 @@ $("#carousel-thumbs-slider-1,#carousel-thumbs-slider-2,#carousel-thumbs-slider-3
   },
   allowPageScroll:"vertical"
 });
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 7,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+
+});
+
+$(document).ready(function () {
+    $(".testimonial-carousel").addClass('owl-carousel').owlCarousel({
+        items: 1,
+        // nav: true,
+        stagePadding: 0,
+        loop: true,
+        // margin: 10,
+        autoplay: true,
+        // autoplayTimeout: 3000,
+        // autoplayHoverPause: true
+    });
+
+});
+
+$apartmentItem = $(".apartment-item-small");
+$positionImage = $(".positon__img")
+
+$close = $(".popup__close");
+$overlay = $(".popup-overlay");
+$apartmentItem.on('click', function () {
+    $('body').addClass('my-body-noscroll-class');
+    $('.apartment-popup').fadeIn();
+    $overlay.toggleClass("overlay");
+})
+
+
+$positionImage.on('click', function () {
+    $('body').addClass('my-body-noscroll-class');
+    $('.position-popup').fadeIn();
+    $overlay.toggleClass("overlay");
+})
+
+$close.on('click', function () {
+    $('.popup').fadeOut();
+    $overlay.removeClass("overlay");
+    $('body').removeClass('my-body-noscroll-class');
+})
